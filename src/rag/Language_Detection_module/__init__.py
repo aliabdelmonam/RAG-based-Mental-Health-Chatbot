75674a966f94004de048d3ed0527e0f0f5bc162d
Ali-Abdelmenam <aliabdelmonam37@gmail.com>
2026-05-25 15:05:09 +0300

    add LanguageDetector import to Language_Detection_module and update main __init__.py

---
+++ src/rag/Language_Detection_module/__init__.py
@@ -0,0 +1,1 @@
+from .Language_detector import LanguageDetector
---
+++ src/rag/__init__.py
@@ -0,0 +1,3 @@
+from .Language_Detection_module import LanguageDetector
+from .Intent_Classifier_module import IntentClassifier
+from .Emotion_Classifier_module import EmotionClassifier
