b578e6ed4ff8bdaea9be114bc55bb2a88c4c3cf9
Ali-Abdelmenam <aliabdelmonam37@gmail.com>
2026-05-27 17:33:55 +0300

    just added some env variables

--- src/core/Config.py
+++ src/core/Config.py
@@ -1,14 +1,19 @@
 from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
 # Resolve the absolute path to the .env file at the project root
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 ENV_FILE_PATH = BASE_DIR / ".env"
 
 class Settings(BaseSettings):
     APP_ENV: str = "development"
     GROQ_API_KEY: str
+    HF_API_KEY: str = ""
+    HF_MODEL_DIR: str = "hugging_face_models"
     GENERATION_MODEL_ID: str = "openai/gpt-oss-safeguard-20b"
+    EMBEDDING_MODEL_ID: str
+
+    GENERATION_BACKEND:str
+    EMBEDDING_BACKEND:str
 
     model_config = SettingsConfigDict(
 
