dadc0a2a2c4301a32eed51ce381f7853e4a1675c
Ali Abdelmenam <aliabdelmonam37@gmail.com>
2026-06-03 01:34:14 +0300

    add (config): add gemini and cohere api key

--- src/core/Config.py
+++ src/core/Config.py
@@ -30,7 +30,8 @@ class Settings(BaseSettings):
     QDRANT_API_KEY: str
 
     COLAB_NGROK_URL: str
-    
+    GEMINI_API_KEY:str
+    COHERE_API_KEY:str
     model_config = SettingsConfigDict(
         env_file=ENV_FILE_PATH,
         env_file_encoding="utf-8",
