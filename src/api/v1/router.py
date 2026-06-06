d6649a3b59e9c9707aa4b0c32e94f58a9e29e0e1
Ali Abdelmenam <aliabdelmonam37@gmail.com>
2026-06-06 15:27:18 +0300

    add (router): add router for fastapi

---
+++ src/api/v1/router.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+from src.api.v1.endpoints import health,chat
+
+api_router = APIRouter()
+
+api_router.include_router(health.router, prefix="/health", tags=["Health Check"])
+api_router.include_router(chat.router, prefix="/chat", tags=["Chatbot Interaction"])
