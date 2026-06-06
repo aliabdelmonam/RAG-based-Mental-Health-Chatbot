0dd4a799b2e6dba4b5e3073d2f7cfa7c418e1732
Ali Abdelmenam <aliabdelmonam37@gmail.com>
2026-06-06 19:17:44 +0300

    add (chat): add session_id to be able to handle multi-users at the same time and each one has his own history

--- src/api/v1/endpoints/chat.py
+++ src/api/v1/endpoints/chat.py
@@ -12,6 +12,7 @@ settings = get_settings()
 # Define the JSON structure your endpoint expects
 class ChatQueryRequest(BaseModel):
     user_message: str
+    session_id: str 
 
 # Initialize provider factories (Safe to do globally as they don't open network sockets)
 llm_provider = LLMProviderFactory(settings)
@@ -28,18 +29,27 @@ embedding_client.set_embedding_model(settings.EMBEDDING_MODEL_ID)
 # Create the vector DB client reference globally, but DO NOT call .connect() here!
 qdrant_client = vector_db_provider.create(provider=settings.VECTORDB_BACKEND, embedding=embedding_client)
 
-pipeline = FullPipeline(
-        generation_client=generation_client,
-        embedding_client=embedding_client,
-        vector_db_client=qdrant_client, # Uses the client connected via main.py lifespan
-        collection_name="Normal_chunking",
-        top_k=1,
-    )
+pipelines: dict[str, FullPipeline] = {}
+
+
+def get_pipeline(session_id: str) -> FullPipeline:
+    if session_id not in pipelines:
+        pipelines[session_id] = FullPipeline(
+            generation_client=generation_client,
+            embedding_client=embedding_client,
+            vector_db_client=qdrant_client,
+            collection_name="Normal_chunking",
+            top_k=1,
+        )
+    return pipelines[session_id]
+
+
 @router.post("/query")
 async def ask_chatbot(request_body: ChatQueryRequest):
     """
     Endpoint to interact with the RAG-based mental health chatbot.
     """
+    pipeline = get_pipeline(request_body.session_id)
     result = pipeline.run(query=request_body.user_message)
 
     return {"response": result}
