a90c211b1e072b281ad42e9b4508a34c9aeed758
Ali Abdelmenam <aliabdelmonam37@gmail.com>
2026-06-06 15:28:06 +0300

    add (vector-db-factory): edd embedding in create function

--- src/db/vector_db_factory.py
+++ src/db/vector_db_factory.py
@@ -1,5 +1,7 @@
 from .VectorDBENUM import VectorDBEnum
 from .providers import ChromaProvider, QDrantProvider
+from langchain_core.embeddings import Embeddings
+from typing import Optional
 
 
 class VectorDBFactory:
@@ -7,12 +9,12 @@ class VectorDBFactory:
     def __init__(self,config):
         self.config = config
 
-    def create(self,provider:VectorDBEnum):
+    def create(self, provider: VectorDBEnum, embedding: Optional[Embeddings] = None):
         if provider == VectorDBEnum.CHROMA.value:
             return ChromaProvider()
         elif provider == VectorDBEnum.QDRANT.value:
             return QDrantProvider(url=self.config.QDRANT_URL,api_key=self.config.QDRANT_API_KEY
                                   ,path=self.config.QDRANT_PATH,in_memory=self.config.QDRANT_IN_MEMORY,
-                                  embedding=None)
+                                  embedding=embedding)
         else:
             raise ValueError(f"Unknown provider: {provider}")
